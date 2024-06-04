1c7023a92ec6579d69ccd8b265fc6e374a7c7306
Philip Koch <philipkoch@live.de>
2024-06-05 00:13:28 +0200

    fixed formatting in neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,27 +1,27 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-neotest/neotest-python",
-    },
-    lazy = false,
-    config = function()
-        require("neotest").setup({
-            quickfix = {
-                open = false,
-            },
-            adapters = {
-                require("neotest-python")({
-                    dap = { justMyCode = false },
-                    args = { "--cov", "-vvv", "--ignore=tests/resources/integration" },
-                }),
-            },
-            status = {
-                virtual_text = false,
-                signs = true,
-            },
-        })
-    end,
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-neotest/neotest-python",
+	},
+	lazy = false,
+	config = function()
+		require("neotest").setup({
+			quickfix = {
+				open = false,
+			},
+			adapters = {
+				require("neotest-python")({
+					dap = { justMyCode = false },
+					args = { "--cov", "-vvv", "--ignore=tests/resources/integration" },
+				}),
+			},
+			status = {
+				virtual_text = false,
+				signs = true,
+			},
+		})
+	end,
 }
