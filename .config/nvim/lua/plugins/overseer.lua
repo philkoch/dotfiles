8de680bc4bcaa55b677054febb5d6b0d53d79fdc
Philip Koch <philipkoch@live.de>
2025-01-31 00:12:08 +0100

    added overseer-nvim

---
+++ .config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/overseer.nvim",
+	opts = {},
+	keys = {
+		{
+			"<leader>rr",
+			function()
+				require("overseer").run_template()
+			end,
+			desc = "Template ...",
+		},
+		{
+			"<leader>rj",
+			function()
+				require("overseer").run_template({ name = "just qa", params = { args = {} } })
+			end,
+			desc = "Just QA-recipe",
+		},
+		{
+			"<leader>rt",
+			function()
+				require("overseer").toggle()
+			end,
+			desc = "Task View",
+		},
+	},
+}
