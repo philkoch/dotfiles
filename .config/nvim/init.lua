de75b1b09722fa688ad78ab051523a64438736f8
Philip Koch <philipkoch@live.de>
2025-02-01 19:24:21 +0100

    removed remains of indent-blankline plugin from init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -234,13 +234,3 @@ require("lazy").setup("plugins", { defaults = { lazy = false } })
 -- COLORSCHEME >>>>>>>
 -- set colorscheme AFTER loading plugins
 vim.cmd.colorscheme("kanagawa-wave")
--- highlights for indent-blankline plugin
-vim.opt.termguicolors = true
-vim.opt.list = true
-
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { fg = "#E06C75", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { fg = "#E5C07B", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent3", { fg = "#98C379", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent4", { fg = "#56B6C2", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent5", { fg = "#61AFEF", nocombine = true })
-vim.api.nvim_set_hl(0, "IndentBlanklineIndent6", { fg = "#C678DD", nocombine = true })
