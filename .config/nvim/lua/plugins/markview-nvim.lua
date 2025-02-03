540e7f4257c5107555a86dcff0d2c0dd5a1f440a
Philip Koch <philipkoch@live.de>
2025-02-03 14:45:28 +0100

    removed markview-nvim

--- .config/nvim/lua/plugins/markview-nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"OXY2DEV/markview.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
-	},
-	ft = "markdown", -- If you decide to lazy-load anyway
-}
