51330b5a9dae71aa15e8b9dd2601b9955a4cf423
Philip Koch <philipkoch@live.de>
2025-05-15 23:39:45 +0200

    nvim: added autocmd for tabwidth of 2 in markdown files. The linter thinks that's best

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 
 -- open :help in vertical split
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	group = vim.api.nvim_create_augroup("help_window_right", {}),
+	group = vim.api.nvim_create_augroup("help_window_right", { clear = true }),
 	pattern = { "*.txt" },
 	callback = function()
 		if vim.o.filetype == "help" then
@@ -13,8 +13,19 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 
 -- highlighted yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+	end,
+})
+
+-- 2-space-tabs in markdown files to be in-line with the linter
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	group = vim.api.nvim_create_augroup("MarkdownTabWidth", { clear = true }),
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.softtabstop = 2
 	end,
 })
