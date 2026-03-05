54a2f90d8eb962d39b8f3d90e6dd4836873ba0ce
Philip Koch <philipkoch@live.de>
2026-03-05 16:07:18 +0100

    neovim: updated lualine configs

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,19 +3,25 @@ return {
 	dependencies = { "kyazdani42/nvim-web-devicons", lazy = true },
 	lazy = false,
 	config = function()
+		local function wide_enough(min_width)
+			return function()
+				return vim.fn.winwidth(0) > min_width
+			end
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
 				},
 				ignore_focus = {},
 				always_divide_middle = true,
-				globalstatus = false,
+				globalstatus = true,
 				refresh = {
 					statusline = 1000,
 					tabline = 1000,
@@ -25,7 +31,7 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = {},
 				lualine_x = {
 					{
 						"encoding",
@@ -34,6 +40,7 @@ return {
 							local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
 							return ret
 						end,
+						cond = wide_enough(80),
 					},
 					{
 						"fileformat",
@@ -42,23 +49,31 @@ return {
 							local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
 							return ret
 						end,
+						cond = wide_enough(80),
 					},
-					"filetype",
+					{ "filetype", cond = wide_enough(60) },
 				},
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
-			inactive_sections = {
+			inactive_sections = {},
+			tabline = {},
+			winbar = {
 				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
+				lualine_b = { { "filename", path = 1 } },
+				lualine_c = { "diagnostics" },
+				lualine_x = {},
 				lualine_y = {},
 				lualine_z = {},
 			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
+			inactive_winbar = {
+				lualine_a = {},
+				lualine_b = { { "filename", path = 1 } },
+				lualine_c = { "diagnostics" },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
 			extensions = { "quickfix" },
 		})
 	end,
